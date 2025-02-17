e87e1761343fef12d131cd3c6e8515860b20eb36
HSJ <sjhan1@naver.com>
2025-02-17 23:39:10 +0900

    [BOJ] / 바구니 뒤집기 / 10811 / Bronze II

---
+++ 알고리즘/week3/HSJ/BOJ10811.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+basket = [i for i in range(1, N+1)]
+for _ in range(M):
+  i, j = map(int, input().split())
+  new = basket[i-1:j]
+  new.reverse()
+  basket[i-1:j] = new
+
+  print(*basket)
